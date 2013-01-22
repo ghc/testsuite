dd093344292f73e24e058532fe9d498f63faa4eb
Ian Lynagh <ian@well-typed.com>
2013-01-22 23:44:31 +0000

    Add a (currently very permissive) test name check
    We now get a framework failure if a test name doesn't match
    ^[a-zA-Z0-9][a-zA-Z0-9._/-]*$

--- driver/testlib.py
+++ driver/testlib.py
@@ -562,6 +562,8 @@ def test (name, setup, func, args):
     global allTestNames
     if name in allTestNames:
         framework_fail(name, 'duplicate', 'There are multiple tests with this name')
+    if not re.match('^[a-zA-Z0-9][a-zA-Z0-9._/-]*$', name):
+        framework_fail(name, 'bad_name', 'This test has an invalid name')
     myTestOpts = copy.copy(thisdir_testopts)
     allTests += [lambda : runTest(myTestOpts, name, setup, func, args)]
     allTestNames.add(name)
