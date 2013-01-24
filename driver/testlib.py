38c3df910d223f61f8122e38f9ed7a7f4f5175af
Ian Lynagh <ian@well-typed.com>
2013-01-24 18:36:46 +0000

    MERGED: Use .hpc.<testname> for the hpc directory
    All tests used to use .hpc, which caused failures when running in
    parallel.

--- driver/testlib.py
+++ driver/testlib.py
@@ -1097,7 +1097,7 @@ def simple_build( name, way, extra_hc_opts, should_fail, top_mod, link, addsuf, 
           + config.compiler + "' " \
           + join(comp_flags,' ') + ' ' \
           + to_do + ' ' + srcname + ' ' \
-          + join(config.way_flags[way],' ') + ' ' \
+          + join(config.way_flags(name)[way],' ') + ' ' \
           + extra_hc_opts + ' ' \
           + opts.extra_hc_opts + ' ' \
           + '>' + errname + ' 2>&1'
@@ -1277,7 +1277,7 @@ def interpreter_run( name, way, extra_hc_opts, compile_only, top_mod ):
     cmd = "'" + config.compiler + "' " \
           + join(getTestOpts().compiler_always_flags,' ') + ' ' \
           + srcname + ' ' \
-          + join(config.way_flags[way],' ') + ' ' \
+          + join(config.way_flags(name)[way],' ') + ' ' \
           + extra_hc_opts + ' ' \
           + getTestOpts().extra_hc_opts + ' ' \
           + '<' + scriptname +  ' 1>' + outname + ' 2>' + errname
@@ -1370,7 +1370,7 @@ def extcore_run( name, way, extra_hc_opts, compile_only, top_mod ):
     cmd = 'cd ' + getTestOpts().testdir + " && '" \
           + config.compiler + "' " \
           + join(getTestOpts().compiler_always_flags,' ') + ' ' \
-          + join(config.way_flags[way],' ') + ' ' \
+          + join(config.way_flags(name)[way],' ') + ' ' \
           + extra_hc_opts + ' ' \
           + getTestOpts().extra_hc_opts \
           + to_do \
@@ -1395,7 +1395,7 @@ def extcore_run( name, way, extra_hc_opts, compile_only, top_mod ):
         deplist2 = string.replace(deplist,'.lhs,', '.hcr');
         to_compile = string.replace(deplist2,'.hs,', '.hcr');
 
-    flags = join(filter(lambda f: f != '-fext-core',config.way_flags[way]),' ')
+    flags = join(filter(lambda f: f != '-fext-core',config.way_flags(name)[way]),' ')
 
     cmd = 'cd ' + getTestOpts().testdir + " && '" \
           + config.compiler + "' " \
