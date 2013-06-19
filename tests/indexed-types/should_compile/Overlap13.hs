d6e010f17bc63fb68c823b931d9af622e78f617c
Richard Eisenberg <eir@cis.upenn.edu>
2013-06-19 10:29:02 +0100

    Update to support new syntax for ordered overlapping type families

--- tests/indexed-types/should_compile/Overlap13.hs
+++ tests/indexed-types/should_compile/Overlap13.hs
@@ -1,13 +1,14 @@
 {-# LANGUAGE TypeFamilies #-}
 
-module Overlap10 where
+module Overlap13 where
 
 type family F a b where
   F a a = Int
+  F Int Int = Int
   F a b = b
 
-g :: a -> F a Bool
-g x = False
+g :: a -> F a Int
+g x = (5 :: Int)
 
 
 
